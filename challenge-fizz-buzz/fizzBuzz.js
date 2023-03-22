3ee59f8516066a4ef151e5bdd9e53bfb152da78f
J4master <128415876+J4master@users.noreply.github.com>
2023-03-22 11:04:39 -0600

    Update fizzBuzz.js

--- challenge-fizz-buzz/fizzBuzz.js
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -1,7 +1,7 @@
 // Write your solution below this line:
 for (let i = 1; i <= 50; i++) {
-    if (i % 3 === 0 && i % 5 ===0) {console.log("FuckYou")}
-    else if (i % 3 === 0) {console.log("Fuck")}
-    else if (i % 5 === 0) {console.log("You")}
+    if (i % 3 === 0 && i % 5 ===0) {console.log("FizzBuzz")}
+    else if (i % 3 === 0) {console.log("Fizz")}
+    else if (i % 5 === 0) {console.log("Buzz")}
     else (console.log(i))
 }
