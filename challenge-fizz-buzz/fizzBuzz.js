f58e956cb5813a6b759a966569de4c1f2a0c022c
Justin Foremaster <justin.foremaster@gmail.com>
2023-03-22 11:00:40 -0600

    initial commit

---
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -0,0 +1,7 @@
+// Write your solution below this line:
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 ===0) {console.log("FuckYou")}
+    else if (i % 3 === 0) {console.log("Fuck")}
+    else if (i % 5 === 0) {console.log("You")}
+    else (console.log(i))
+}
